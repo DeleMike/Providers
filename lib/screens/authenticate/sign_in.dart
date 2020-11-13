1dd11f79ace4b6d7a0c38d03d873d3746593fe72
DeleMike <akindelemichael65@gmail.com>
2020-11-13 11:16:34 +0100

    'designing text form field'

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:jrup/changer.dart';
 import 'package:provider/provider.dart';
 
 class SignIn extends StatefulWidget {
@@ -7,63 +6,49 @@ class SignIn extends StatefulWidget {
   _SignInState createState() => _SignInState();
 }
 
-class _SignInState extends State<SignIn> {
-
-  Changer _changerVal;
+class _SignInState extends State<SignIn> { 
 
   @override
   Widget build(BuildContext context) {
-    final val = Provider.of<Changer>(context);
+     //print("ChangerVal initial value = ${_changerVal.heightVal}");
+    // final val = Provider.of<Changer>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Sign in into app'),
-      ),
-      body: Container(
-        child: Center(
-          child: Card(
-            margin: const EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 6.0),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'Name'
-                    ),
+        appBar: AppBar(
+    title: Text('Sign in into app'),
+        ),
+        body: Container(
+    child: Center(
+      child: Card(
+        margin: const EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 6.0),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Name',
+                  fillColor: Colors.indigo[100],
+                ),
 
-                  ),
-
-                  SizedBox(height: 25.0),
-
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'Password'
-                    ),
-                  ),
-
-                  Slider(
-                    value: (_changerVal.heightVal ?? 100),
-                    activeColor: Colors.indigo[_changerVal.heightVal ?? 100],
-                    inactiveColor: Colors.indigo[_changerVal.heightVal ?? 100],
-                    min: 100.0,
-                    max: 900.0,
-                    divisions: 8,
-                    onChanged: (val) {
-                      setState((){_changerVal.heightVal = val;});
-                      print('_changerVal = ${_changerVal.heightVal}');
-                    }
-                        // setState(() => _currentStrength = val.round()),
-                  ),
-                ]
               ),
-              
-            ),
-            elevation: 4.0,
+
+              SizedBox(height: 25.0),
+
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Password'
+                ),
+              ),
+            ]
           ),
           
         ),
+        elevation: 4.0,
       ),
-    );
+      
+    ),
+        ),
+      );
   }
 }
