b0b65555d56f0c2dd6742568a80d2380e62b5437
DeleMike <akindelemichael65@gmail.com>
2020-11-14 16:23:08 +0100

    working on provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:jrup/flavor.dart';
+import 'package:provider/provider.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+  runApp(Provider<Flavor>.value(
+    value: Flavor.dev, //reads value from here
+    child: MyApp()
+  ));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final flavor = Provider.of<Flavor>(context);
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: flavor.toString(),
       theme: ThemeData(
         primarySwatch: Colors.indigo,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
-        child: Container(
+        body : Container(
           child: Center(
-            child: Text('Hello');
+            child: Text('Hello'),
           ),
         ),
       ),
