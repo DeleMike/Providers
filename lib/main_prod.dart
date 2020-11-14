0481d8513aa1d7746f289c7e2ffe46fb543f880f
DeleMike <akindelemichael65@gmail.com>
2020-11-14 16:47:57 +0100

    deleted unwanted classes

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,13 +1,13 @@
-import 'package:flutter/material.dart';
-import 'package:jrup/flavor.dart';
-import 'package:jrup/my_app.dart';
-import 'package:provider/provider.dart';
+// import 'package:flutter/material.dart';
+// import 'package:jrup/flavor.dart';
+// import 'package:jrup/my_app.dart';
+// import 'package:provider/provider.dart';
 
-void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
-  runApp(Provider<Flavor>.value(
-    value: Flavor.prod, //reads value from here
-    child: MyApp()
-  ));
-}
+// void main() {
+//   // WidgetsFlutterBinding.ensureInitialized();
+//   // await Firebase.initializeApp();
+//   runApp(Provider<Flavor>.value(
+//     value: Flavor.prod, //reads value from here
+//     child: MyApp()
+//   ));
+// }
