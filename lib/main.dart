5ddb2b4d2a7be46b400f04b9f00791b1b0322947
DeleMike <akindelemichael65@gmail.com>
2020-11-16 13:17:06 +0100

    use stream values in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,37 +3,57 @@ import 'package:jrup/Valuer.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    StreamProvider<int>(
+      create: (_) => Valuer(initialValue: 100).value,
+      initialData: 100,
+      catchError: (_, error) => error,
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print('widegt built here');
-    return StreamProvider(
-        create: (_) => Valuer().value,
-          child: Center(
-        child: Column(
-          children: [
-            Consumer<Valuer>(
-              builder: (_, valuer, __) =>
-                          Container(
-                padding: EdgeInsets.all(8.0),
-                margin: EdgeInsets.all(8.0),
-                color: Colors.blue[valuer.initialValue],
-              ),
-            ),
+    print('widget built here');
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
 
-            Consumer<Valuer>(
-              builder: (_, valuer, __) =>
-                           ElevatedButton(
-                child: Text('Reset'),
-                onPressed: () {
-                  valuer.reset();
-                },
+class HomePage extends StatelessWidget {
+  final String title;
+  HomePage({this.title});
+  @override
+  Widget build(BuildContext context) {
+    //var countVal = Provider.of<Valuer>(context);
+    //print('Value = ${countVal.initialValue}');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Consumer<int>(
+          builder: (_, int value, __) => Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(8.0),
+                margin: EdgeInsets.all(16.0),
+                height: 250.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blueGrey[value],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
