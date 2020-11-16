703fd577a09c9dbec959f7731d3b54d51db8c132
DeleMike <akindelemichael65@gmail.com>
2020-11-16 10:16:35 +0100

    add code to change button state

--- lib/changer.dart
+++ lib/changer.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/foundation.dart';
 
-class Changer with ChangeNotifier{
+class Changer with ChangeNotifier {
   double value = 700.0;
   bool isVisible = false;
+  String text;
+
+  Changer() {
+    text = isVisible ? 'Hide' : 'Show';
+  }
 
   void increment(val) {
-    value = val ;
+    value = val;
     notifyListeners();
   }
 
