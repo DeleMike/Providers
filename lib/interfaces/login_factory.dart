823cc3543babf66cdd9ec783197be86660e4dc11
DeleMike <akindelemichael65@gmail.com>
2020-11-16 15:18:56 +0100

    add factory class

---
+++ lib/interfaces/login_factory.dart
@@ -0,0 +1,13 @@
+class LoginFactory{
+   ///allows user to sign in into their account
+  void signInWithEmailAndPassword() {}
+
+  //sign in with Google Provider
+
+  ///allows user to sign out from their account
+  void signOut() {}
+
+  ///allows user to reset password upon forgeting their passoword
+  void forgotPassword() {}
+
+}
---
+++ lib/interfaces/register_factory.dart
@@ -0,0 +1,8 @@
+class RegisterFactory{
+   ///allows user to register a new account
+  void registerWithEmailAndPassword() {}
+
+  //register with Google Provider
+
+
+}
