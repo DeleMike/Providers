1a4e59170265a806ceb8c57f6a3ab16b60390a6d
DeleMike <akindelemichael65@gmail.com>
2020-11-16 15:48:00 +0100

    add sign out function

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:jrup/services/auth.dart';
 
 class Home extends StatelessWidget {
+  final AuthService _authService = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.indigo[50],
       appBar: AppBar(
         title: Text('Home Screen'),
+        actions: [
+          FlatButton.icon(
+            onPressed: () async {
+              var value = await _authService.signOut();
+              if (value != null) {
+                Scaffold.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Something went wrong, could not sign out'),
+                  ),
+                );
+              }
+            },
+            icon: Icon(Icons.person),
+            label: Text(
+              'logout',
+            ),
+          ),
+        ],
       ),
       body: Center(
         child: Container(
