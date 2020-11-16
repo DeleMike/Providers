a847a0b3956daa3d60579691d34ac2bcdb9cd658
DeleMike <akindelemichael65@gmail.com>
2020-11-16 11:37:32 +0100

    configure main.dart to use provider widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pie_chart/pie_chart.dart';
+import 'package:jrup/Valuer.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -9,10 +9,32 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        padding: EdgeInsets.all(8.0),
-        margin: EdgeInsets.all(8.0),
+    print('widegt built here');
+    return StreamProvider(
+        create: (_) => Valuer().value,
+          child: Center(
+        child: Column(
+          children: [
+            Consumer<Valuer>(
+              builder: (_, valuer, __) =>
+                          Container(
+                padding: EdgeInsets.all(8.0),
+                margin: EdgeInsets.all(8.0),
+                color: Colors.blue[valuer.initialValue],
+              ),
+            ),
+
+            Consumer<Valuer>(
+              builder: (_, valuer, __) =>
+                           ElevatedButton(
+                child: Text('Reset'),
+                onPressed: () {
+                  valuer.reset();
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
