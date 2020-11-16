553992d9337fc2fd1fb49c696954d94a021d79df
DeleMike <akindelemichael65@gmail.com>
2020-11-16 16:08:12 +0100

    use Provider API to get auth state value

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:jrup/models/user.dart';
 import 'package:jrup/screens/authenticate/authenticate.dart';
+import 'package:jrup/screens/home/home.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Authenticate();
+    final userAuthState = Provider.of<User>(context);
+
+    //return either Authenticate widget or Home widget
+    if (userAuthState == null) {
+      return Authenticate();
+    } else {
+      return Home();
+    }
   }
 }
