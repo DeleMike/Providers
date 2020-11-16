fd529ba7019bd7ea3a999a7f59c16e63be3135eb
DeleMike <akindelemichael65@gmail.com>
2020-11-16 16:05:27 +0100

    update: toggle authentication screens

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:jrup/screens/authenticate/sign_in.dart';
+import 'package:jrup/screens/authenticate/sign_up.dart';
 
 
-class Authenticate extends StatelessWidget {
+class Authenticate extends StatefulWidget {
+
+  @override
+  _AuthenticateState createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+  bool showSignIn = true;
+
+  void toggleView() {
+    setState(() => showSignIn = !showSignIn);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SignIn();
+    if (showSignIn) {
+      return SignIn(toggleView: toggleView);
+    }else {
+      return SignUp(toggleView: toggleView);
+    }
   }
 }
