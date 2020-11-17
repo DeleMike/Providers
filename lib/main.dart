5db2a3bad5a55d7b59a3fd50c3639d06e70f41cd
DeleMike <akindelemichael65@gmail.com>
2020-11-17 15:19:04 +0100

    add new route

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:jrup/models/user.dart';
 import 'package:jrup/screens/authenticate/authenticate.dart';
+import 'package:jrup/screens/authenticate/forgot_password.dart';
 import 'package:jrup/screens/home/home.dart';
 import 'package:jrup/screens/wrapper.dart';
 import 'package:jrup/services/auth.dart';
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
   //routes
   static final String authenticatePage = '/authenticatePage';
   static final String homePage = '/homePage';
+  static final String forgotPassword = '/forgotPassword';
  
   @override
   Widget build(BuildContext context) {
@@ -33,6 +35,7 @@ class MyApp extends StatelessWidget {
           '/' : (context) => Wrapper(),
           authenticatePage: (context) => Authenticate(),
           homePage: (context) => Home(),
+          forgotPassword : (context) => ForgotPassword(),
         },
         debugShowCheckedModeBanner: false,
       ),
