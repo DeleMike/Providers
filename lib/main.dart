6d985d89c03ede6b58a32ac2e9bd1fd93293a7d4
DeleMike <akindelemichael65@gmail.com>
2020-11-18 11:37:30 +0100

    add outlinebutton themedata

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,9 @@ class MyApp extends StatelessWidget {
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.indigo,
+          outlinedButtonTheme: OutlinedButtonThemeData(
+            style: OutlinedButton.styleFrom(primary: Colors.indigo),
+          ),
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
         initialRoute: '/',
