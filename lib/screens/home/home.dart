e2c93c8687f7aa07b08b647327810664a1cd2919
DeleMike <akindelemichael65@gmail.com>
2020-11-21 16:24:21 +0100

    refactor db

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:jrup/models/user.dart';
-import 'package:jrup/screens/home/create_room_form.dart';
 import 'package:jrup/services/auth.dart';
 import 'package:jrup/shared/unverified.dart';
 import 'package:provider/provider.dart';
@@ -24,37 +23,13 @@ class Home extends StatelessWidget {
 
     return userIsVerified == true
         ? Scaffold(
-            resizeToAvoidBottomInset: false,
             backgroundColor: Colors.indigo[50],
             appBar: AppBar(
               title: Text('Home Screen'),
-              actions: [
-                FlatButton.icon(
-                  onPressed: () async {
-                    var value = await _authService.signOut();
-                    if (value != null) {
-                      Scaffold.of(context).showSnackBar(
-                        SnackBar(
-                          content:
-                              Text('Something went wrong, could not sign out'),
-                        ),
-                      );
-                    }
-                  },
-                  icon: Icon(
-                    Icons.person,
-                    color: Colors.white,
-                  ),
-                  label: Text(
-                    'logout',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ],
             ),
-            body: Container(),
+            body: Center(
+              child: Text('Shop items'),
+            ),
           )
         : Unverified(
             title: 'ACCOUNT NOT VERIFIED',
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,42 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
-  final String password;
   //a room reference
-  CollectionReference courseCollection =
-      FirebaseFirestore.instance.collection('courses');
+  CollectionReference courseCollection = FirebaseFirestore.instance.collection('orders');
 
-  //KEYS
-  final String mCourseName = 'courseName';
-  final String mCourseKey = 'courseKey';
-  // final String mCourseExamsStartDate = 'courseExamsStartDate';
-  // final String mCourseExamsEndDate = 'courseExamsEndDate';
-  // final String mCourseExamsStartTime = 'courseExamsStartTime';
-  // final String mCourseExamsEndTime = 'courseExamsEndTime';
-  // final String mCourseExamsTimeLimit = 'courseExamsTimeLimit';
-  // final String mShowResults = 'showResults';
-  // final String mViewRightAnswers = 'course';
-
-  DatabaseService({this.password});
-
-  Future createNewCourse(String name, String password) async {
-    return await courseCollection.doc(password).set({
-      mCourseName: name,
-      mCourseKey: password,
-    });
-  }
-
-  // //create course
-  // Future createNewCourse(
-  //     {@required String name,
-  //     @required String key,
-  //     @required String examsStartDate,
-  //     @required String examsEndDate,
-  //     @required String examsStartTime,
-  //     @required String examsEndTime,
-  //     @required String examsTimeLimit,
-  //     @required bool showResults,
-  //     @required bool viewRightAnswers}) async {
-
-  // }
 }
